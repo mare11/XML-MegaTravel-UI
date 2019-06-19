c6913de7a78cd6d48e43c2569901e869dacd48ce
mare11 <44523703+mare11@users.noreply.github.com>
2019-06-19 16:43:56 +0200

    add user login and registration

--- user/src/app/app-routing.module.ts
+++ user/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { VerifyUserComponent } from './components/verify-user/verify-user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'registration/verify', component: VerifyUserComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- user/src/app/app.module.ts
+++ user/src/app/app.module.ts
@@ -3,14 +3,35 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { MaterialModule } from './material';
+import { HttpClientModule } from '@angular/common/http';
+import { LoginDialogComponent } from './components/login-dialog/login-dialog.component';
+import { RegistrationDialogComponent } from './components/registration-dialog/registration-dialog.component';
+import { VerifyUserComponent } from './components/verify-user/verify-user.component';
+import { SnackBar } from './utils';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginDialogComponent,
+    RegistrationDialogComponent,
+    VerifyUserComponent
+  ],
+  entryComponents: [
+    LoginDialogComponent,
+    RegistrationDialogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    MaterialModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    SnackBar
   ],
   providers: [],
   bootstrap: [AppComponent]
